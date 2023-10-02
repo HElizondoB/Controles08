bd6fabc41d9cb5d0ae51ecb87264f58868524cd2
Humberto Elizondo <heb.humberto@live.com>
2023-10-01 21:48:00 -0700

    MoreChanges

---
+++ Migrations/20231002044012_mssql_migration_756.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Controles08.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssql_migration_756 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
